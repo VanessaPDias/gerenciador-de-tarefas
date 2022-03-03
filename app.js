71b2f8226a4c52ede5e6fc157054ee6654bbadb1
Vanessa Dias <vanevanessa.dias@gmail.com>
2022-03-03 17:48:39 +0000

    criação do input para criar tarefa

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-
-
-const usuario = JSON.parse(localStorage.getItem("logado"));
-
-if (usuario.logado !== true) {
-    window.location.href = "/entrar.html"
-};
+verificarSeUsuarioEstaLogado();
 
 window.onload = iniciar;
 
-function iniciar() {
-    const avatar = document.querySelector("#avatar")
-    const nomeUsuario = document.querySelector("#nomeUsuario");
+function verificarSeUsuarioEstaLogado() {
+    const usuario = JSON.parse(localStorage.getItem("logado"));
 
-    const nomeCompleto = usuario.nome;
-    const primeiroNome = nomeCompleto.split(" ", 1);
-    
-    nomeUsuario.innerHTML = primeiroNome;
+    if (usuario.logado !== true) {
+        window.location.href = "/entrar.html";
+    };
+}
+
+function iniciar() {
+    imprimirNomeDoUsuario();
 
     document.querySelector("#btnSair").onclick = sair;
 }
 
+
+function imprimirNomeDoUsuario() {
+    const nomeUsuario = document.querySelector("#nomeUsuario");
+
+    const nomeCompleto = usuario.nome;
+    const primeiroNome = nomeCompleto.split(" ", 1);
+
+    nomeUsuario.innerHTML = primeiroNome;
+}
 
 function sair() {
     localStorage.removeItem("logado");
