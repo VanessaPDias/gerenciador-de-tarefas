182013dec6a98682ebc32d61d957df861b30a252
Vanessa Dias <vanevanessa.dias@gmail.com>
2022-03-08 14:30:36 +0000

    inclusão do evento onclick no checkbox que marca a tarefa como concluida

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const usuario = recuperarUsuarioLogado();
 
-window.onload = iniciar;
-
 function recuperarUsuarioLogado() {
     const usuarioEncontrado = JSON.parse(localStorage.getItem("logado"));
 
-    if (usuarioEncontrado == null || usuarioEncontrado.logado !== true ) {
+    if (usuarioEncontrado == null || usuarioEncontrado.logado !== true) {
         window.location.href = "/entrar.html";
     };
-
     return usuarioEncontrado;
 }
 
+window.onload = iniciar;
+
 function iniciar() {
     imprimirNomeDoUsuario();
-
+    recuperarListaDeTarefas();
+    adicionarEventoCheckBox()
+    
     document.querySelector("#btn-sair").onclick = sair;
-
+    
     document.querySelector("#criar-tarefa").onclick = criarTarefa;
+    
+    
 }
-
 
 function imprimirNomeDoUsuario() {
     const nomeUsuario = document.querySelector("#nome-usuario");
@@ -30,6 +32,40 @@ function imprimirNomeDoUsuario() {
     nomeUsuario.innerHTML = primeiroNome;
 }
 
+
+function recuperarListaDeTarefas() {
+    const listaEncontrada = JSON.parse(localStorage.getItem(usuario.email + "_tarefas"));
+
+    if (listaEncontrada !== null) {
+        imprimirListaDeTarefas()
+    }
+}
+
+
+function imprimirListaDeTarefas() {
+    const chave = usuario.email + "_tarefas";
+    const listaDeTarefas = JSON.parse(localStorage.getItem(chave));
+    
+    
+    listaDeTarefas.tarefas.forEach(element => {
+        document.querySelector("#lista-de-tarefas").innerHTML = document.querySelector("#lista-de-tarefas").innerHTML + 
+        `<li class="tarefa list-group-item mb-2 d-flex">
+        <input class="form-check-input me-2 btn-marcar" type="checkbox" value="" >
+        <a href="#" class="flex-grow-1 link-dark text-decoration-none">${element}</a>
+        <a href="" class="" id="btn-prioridade"><i class="bi bi-star link-dark"></i></a>
+        </li>`;
+        adicionarEventoCheckBox()
+    });
+    
+}
+
+function adicionarEventoCheckBox() {
+    const listaCheckBox = document.querySelectorAll(".btn-marcar");
+    listaCheckBox.forEach(element => {
+        element.onclick = concluirTarefa;
+    });
+}
+
 function sair() {
     localStorage.removeItem("logado");
     window.location.href = "/index.html"
@@ -38,25 +74,49 @@ function sair() {
 
 function criarTarefa() {
     const inputTarefa = document.querySelector("#input-tarefa").value;
-
     const chave = usuario.email + "_tarefas";
 
-    if(localStorage.getItem(chave) == null) {
-        const listaDeTarefas = { 
+    if (localStorage.getItem(chave) == null) {
+        const listaDeTarefas = {
             tarefas: []
         };
 
-       listaDeTarefas.tarefas.push(inputTarefa);
+        listaDeTarefas.tarefas.push(inputTarefa);
 
-        localStorage.setItem(chave, JSON.stringify(jh));
+        localStorage.setItem(chave, JSON.stringify(listaDeTarefas));
+
+        imprimirListaDeTarefas();
+
     } else {
         const listaDeTarefas = JSON.parse(localStorage.getItem(chave));
         listaDeTarefas.tarefas.push(inputTarefa);
         localStorage.setItem(chave, JSON.stringify(listaDeTarefas));
+        document.querySelector("#lista-de-tarefas").innerHTML = ""
+        imprimirListaDeTarefas();
+    }
+}
+
+// function listarTarefas(chave) {
+//     const listaDeTarefas = JSON.parse(localStorage.getItem(chave));
+
+//     listaDeTarefas.tarefas.forEach(element => {
+//         console.log(element)
+
+//         document.querySelector("#lista-de-tarefas").innerHTML = document.querySelector("#lista-de-tarefas").innerHTML + `<li class="tarefa list-group-item">${element}</li>`;
+//     });
+
+//}
+
+function concluirTarefa(evento) {
+    if(evento.target.checked == true){
+        evento.target.parentElement.classList.add("tarefa-concluida");
+    } else {
+        evento.target.parentElement.classList.remove("tarefa-concluida");
     }
 }
 
 
-   
+
+
 
 
