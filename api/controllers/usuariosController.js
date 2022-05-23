27a39a25171644b3ba931a4b5dc0d373c97a239f
Vanessa Dias <vanevanessa.dias@gmail.com>
2022-05-23 12:26:30 +0100

    integração da api com banco de dados - usuarios

--- api/controllers/usuariosController.js
+++ api/controllers/usuariosController.js
@@ -1,8 +1,8 @@
 
-const dados = require('../dados')
+const mySql = require('mysql2');
 
 //geração de identificadores unicos
-const crypto = require('crypto'); 
+const crypto = require('crypto');
 
 function criarUsuario(req, res) {
     let novoUsuario = {
@@ -13,38 +13,72 @@ function criarUsuario(req, res) {
         listaDeTarefas: []
     };
 
-    if(!novoUsuario.email) {
-        res.status(400).send({erro: "Não é possível cadastrar usuário sem email"});
+    if (!novoUsuario.email) {
+        res.status(400).send({ erro: "Não é possível cadastrar usuário sem email" });
         return;
     }
 
 
-    const usuarioEncontrado = dados.usuarios.find(usuario => novoUsuario.email == usuario.email);
+    const conexao = mySql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'admin',
+        database: 'todolist'
+    });
 
-    if(!usuarioEncontrado) {
-        dados.usuarios.push(novoUsuario);
-        res.send({ usuarioId: novoUsuario.id });
-    } else {
-        res.status(400).send({ erro: "Email já foi cadastrado" });
-    }
+    conexao.connect();
+
+    conexao.query(`select UsuarioId from usuarios where email = '${novoUsuario.email}'`, function (error, results, fields) {
+        const usuarioEncontrado = results[0];
+
+        if (!usuarioEncontrado) {
+            conexao.query(`insert into usuarios (UsuarioId, Nome, Email, Senha) 
+            values ('${novoUsuario.id}', '${novoUsuario.nome}', '${novoUsuario.email}', '${novoUsuario.senha}')`, function (error, results, fields) {
+                if(error) {
+                    res.status(500).send({erro: "Houve um erro interno ao tentar execultar a sua operação. Tente mais tarde."});
+                    return;
+                }
+               
+                res.send({ usuarioId: novoUsuario.id });
+                
+            })
+            
+        } else {
+            res.status(400).send({ erro: "Email já foi cadastrado" });
+        }
+    });
+
 
 }
 
 function buscarUsuario(req, res) {
     const id = req.params.id;
-    const usuarioEncontrado = dados.usuarios.find(usuario => id == usuario.id);
 
-    if (!usuarioEncontrado) {
-        res.status(404).send({ erro: "Usuário não encontrado" });
-        return;
-    }
-
-    res.send({
-        id: usuarioEncontrado.id,
-        nome: usuarioEncontrado.nome,
-        email: usuarioEncontrado.email,
-        tarefas:usuarioEncontrado.listaDeTarefas
+    const conexao = mySql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'admin',
+        database: 'todolist'
     });
+
+    conexao.connect();
+
+    conexao.query(`select * from usuarios where UsuarioId = ${id}`, function (error, results, fields) {
+        const usuarioEncontrado = results[0];
+
+        if (!usuarioEncontrado) {
+            res.status(404).send({ erro: "Usuário não encontrado" });
+            return;
+        }
+    
+        res.send({
+            id: usuarioEncontrado.UsuarioId,
+            nome: usuarioEncontrado.Nome,
+            email: usuarioEncontrado.Email,
+            //tarefas: usuarioEncontrado.listaDeTarefas
+        });
+    })
+
 }
 
 module.exports = {
